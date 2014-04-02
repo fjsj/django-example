c9f0d4e969dae4a7c3c23beaa154314f7e334269
Flávio Juvenal <flaviojuvenal@gmail.com>
2014-04-02 20:42:48 -0300

    post-receive

---
+++ post-receive.sh
@@ -0,0 +1,8 @@
+env_python=../env/bin/python
+env_pip=../env/bin/pip
+
+@sudo fuser -k 8080/tcp
+$(env_python) manage.py syncdb --noinput --settings=core.settings.production
+$(env_python) manage.py migrate --settings=core.settings.production
+$(env_python) manage.py collectstatic --noinput --settings=core.settings.production
+$(env_python) manage.py runserver 8080
